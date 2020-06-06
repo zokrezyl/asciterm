6f014d10d073a38964ffb2008b89d36f90d7803e
zokrezyl <zokrezyl@gmail.com>
2020-06-06 10:52:59 +0200

    removed direct dependency on glumpy for font

--- src/asciterm_generic.py
+++ src/asciterm_generic.py
@@ -17,6 +17,7 @@ import queue
 
 from esc_seq_parser import BufferProcessor
 from progman import ProgramManager
+from font import ArtSciTermFont
 
 vertex = """
 #version 120
@@ -132,44 +133,6 @@ fragment1 = """
     } """
 
 
-class ArtSciTermFont:
-    def __init__(self):
-        self.char_width = 6.0
-        self.char_height = 13.0
-        regular = glumpy.data.get("6x13-regular.npy")
-        italic = glumpy.data.get("6x13-italic.npy")
-        bold  = glumpy.data.get("6x13-bold.npy")
-        n1 = len(regular)
-        n2 = len(italic)
-        n3 = len(bold)
-        n = n1+n2+n3
-        dtype = [ ("code", np.uint32, 1),
-                  ("data", np.uint8, 10)]
-        font = np.zeros(n, dtype)
-        font[:n1] = regular
-        font[n1:n1+n2] = italic
-        font[n1:n1+n2]["code"] += 1*65536
-        font[n1+n2:n1+n2+n3] = bold
-        font[n1+n2:n1+n2+n3]["code"] += 2*65536
-        font["data"][0] = font["data"][1] #we set the character of space for zero
-
-        # Build a texture out of glyph arrays (need to unpack bits)
-        # This code is specific for a character size of 6x13
-        n = len(font)
-        G = np.unpackbits(font["data"].ravel())
-        G = G.reshape(n,80)[:,:78].reshape(n,13,6)
-
-        self.t_width, self.t_height = 6*128, 13*((n//128)+1)
-        data = np.zeros((self.t_height, self.t_width), np.ubyte)
-
-        for i in range(n):
-            r = 13*(i//128)
-            c = 6*(i % 128)
-            data[r:r+13,c:c+6] = G[i]*255
-
-        # Store char codes
-        self._codes = font["code"]
-        self.data = data
 
 
 
---
+++ src/font.py
@@ -0,0 +1,51 @@
+import numpy as np
+import os
+
+class ArtSciTermFont:
+    def __init__(self):
+        self_path = os.path.dirname(os.path.abspath(__file__))
+        print(self_path)
+        self.data_dir = os.path.join(self_path, "data")
+
+        self.char_width = 6.0
+        self.char_height = 13.0
+        regular = self.load_font("6x13-regular.npy")
+        italic = self.load_font("6x13-italic.npy")
+        bold  = self.load_font("6x13-bold.npy")
+        n1 = len(regular)
+        n2 = len(italic)
+        n3 = len(bold)
+        n = n1+n2+n3
+        dtype = [ ("code", np.uint32, 1),
+                  ("data", np.uint8, 10)]
+        font = np.zeros(n, dtype)
+        font[:n1] = regular
+        font[n1:n1+n2] = italic
+        font[n1:n1+n2]["code"] += 1*65536
+        font[n1+n2:n1+n2+n3] = bold
+        font[n1+n2:n1+n2+n3]["code"] += 2*65536
+        font["data"][0] = font["data"][1] #we set the character of space for zero
+
+        # Build a texture out of glyph arrays (need to unpack bits)
+        # This code is specific for a character size of 6x13
+        n = len(font)
+        G = np.unpackbits(font["data"].ravel())
+        G = G.reshape(n,80)[:,:78].reshape(n,13,6)
+
+        self.t_width, self.t_height = 6*128, 13*((n//128)+1)
+        data = np.zeros((self.t_height, self.t_width), np.ubyte)
+
+        for i in range(n):
+            r = 13*(i//128)
+            c = 6*(i % 128)
+            data[r:r+13,c:c+6] = G[i]*255
+
+        # Store char codes
+        self._codes = font["code"]
+        self.data = data
+    
+
+    def load_font(self, name):
+        return np.load(os.path.join(self.data_dir, name))
+
+
